96f2d0260bf80e1454fd4df73009b8abbfad9801
Meshwa Patel <patelmeshwa68@gmail.com>
2022-07-19 03:20:27 -0700

    Databinding

--- angular-complete-guide/src/app/recipes/recipe-list/recipe-list.component.ts
+++ angular-complete-guide/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -8,14 +8,20 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
-    new Recipe('Test recipe', 'Its delicious', 'https://images.freeimages.com/images/large-previews/a31/colorful-umbrella-1176220.jpg'),
-    new Recipe('Test recipe', 'Its delicious', 'https://images.freeimages.com/images/large-previews/a31/colorful-umbrella-1176220.jpg'),
-    new Recipe('Test recipe', 'Its delicious', 'https://images.freeimages.com/images/large-previews/a31/colorful-umbrella-1176220.jpg')
+    new Recipe('Test recipe 1', 'Its delicious', 'https://images.freeimages.com/images/large-previews/a31/colorful-umbrella-1176220.jpg'),
+    new Recipe('Test recipe 2', 'Its yum', 'https://images.freeimages.com/images/large-previews/a31/colorful-umbrella-1176220.jpg'),
+    new Recipe('Test recipe 3', 'Its extraordinary', 'https://images.freeimages.com/images/large-previews/a31/colorful-umbrella-1176220.jpg')
   ];
+
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
   }
 
 }
--- angular-complete-guide/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ angular-complete-guide/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, EventEmitter, OnInit, Output, ViewChild } from '@angular/core';
+import { Ingredient } from 'src/app/shared/ingrediet.model';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -6,10 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-edit.component.css']
 })
 export class ShoppingEditComponent implements OnInit {
+  @ViewChild('nameInput', { static: false }) nameInputRef: ElementRef; 
+  @ViewChild('amountInput', { static: false }) amountInputRef: ElementRef; 
+  @Output() ingredientAdded = new EventEmitter<Ingredient>();
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onAddItem() {
+    const name = this.nameInputRef.nativeElement.value;
+    const amount = this.amountInputRef.nativeElement.value;
+    this.ingredientAdded.emit(new Ingredient(name, amount));
   }
 
 }
